9c6ad2d14f9d45e73307335a1e67c384d9f8d0a8
MarinaMuntian <dp060491smv@gmail.com>
2021-09-08 02:31:01 +0300

    update task-1

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,40 @@
+import css from './Profile.module.css';
+import PropTypes from 'prop-types';
+
+export const Profile = ({ name, tag, location, avatar, followers, views, likes }) => {
+  return (
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img src={avatar} alt="Аватар пользователя" className={css.avatar} />
+        <p className={css.name}>{name}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
+
+      <ul className={css.stats}>
+        <li>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>{followers}</span>
+        </li>
+        <li>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>{views}</span>
+        </li>
+        <li>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Profile.propTypes = {
+  name: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  avatar: PropTypes.string,
+  followers: PropTypes.number,
+  views: PropTypes.number,
+  likes: PropTypes.number,
+};
