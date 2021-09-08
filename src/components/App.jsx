23fa1310a67e3361529804e05c8f2e446411a034
MarinaMuntian <dp060491smv@gmail.com>
2021-09-08 18:42:22 +0300

    add task-3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,9 @@ import user from '../user.json';
 import { Statistics } from 'components/Statistics/Statistics';
 import statisticalData from '../statistical-data.json';
 
+import { FriendList } from 'components/FriendList/FriendList';
+import friends from '../friends.json';
+
 export const App = () => {
   return (
     <div>
@@ -16,7 +19,10 @@ export const App = () => {
         views={user.stats.views}
         likes={user.stats.likes}
       />
+
       <Statistics title="Upload stats" stats={statisticalData} />
+
+      <FriendList friends={friends} />
     </div>
   );
 };
